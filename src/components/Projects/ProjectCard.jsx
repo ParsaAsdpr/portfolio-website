90cb74ead97e665fdf959c868e3dc7350ec880c5
Parsa <parsaasadpour999@gmail.com>
2022-09-19 18:57:57 +0430

    added Projects section

---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { FaShare } from "react-icons/fa";
+import Image from "next/image";
+
+const ProjectCard = (props) => {
+  let del;
+
+  if (props.duration > 0) del = props.duration / 10 + 0.1;
+  return (
+    <div
+      className={`flex flex-col project-card w-[400px] sm:w-[300px] md:w-[340px] lg:w-[330px] xl:w-[400px] duration-700 ${
+        props.perc > 0.2
+          ? "opacity-1 translate-x-0"
+          : "opacity-0 translate-x-full"
+      }`}
+      style={{ transitionDelay: `${del}s` }}
+    >
+      <div className="w-full project-container h-[250px] sm:h-[200px] md:h-[210px] lg:h-[200px] xl:h-[240px] rounded-md border border-black overflow-hidden cursor-pointer project-image">
+        <div className="project-mask">
+          <div className="flex flex-row justify-between px-5">
+            <FaShare
+              color="#ffffff"
+              opacity="0.8"
+              className="border border-white rounded-full p-1 text-2xl"
+            />
+          </div>
+        </div>
+        <Image
+          src={props.img}
+          alt="Project preview"
+          draggable={false}
+          className="w-full hover:scale-[1.03] duration-700"
+        />
+      </div>
+      <div className="flex flex-row pt-5 sm:pt-8 md:pt-10 relative px-1 sm:px-2 md:px-3 items-center">
+        <h4 className=" text-base sm:text-lg md:text-xl lg:text-2xl font-bold pr-4 bg-white absolute top-1/2">
+          {props.title}
+        </h4>
+        <div className="border-t border-black w-full"></div>
+      </div>
+      <p className=" mt-5 sm:mt-6 px-1 sm:px-2 md:px-3 text-xs sm:text-sm md:text-base">
+        {props.desc}
+      </p>
+    </div>
+  );
+};
+
+export default ProjectCard;
---
+++ src/components/Projects/ProjectSlider.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Academy from "../../../public/images/Nike Redesign.png";
+import { BsArrowLeft } from "react-icons/bs";
+import { Parallax } from "react-parallax";
+
+const ProjectSlider = () => {
+  const [projects] = React.useState([
+    {
+      title: "Nike Redesign",
+      img: Academy,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus id voluptates debitis molestias accusantium?",
+    },
+    {
+      title: "Nike Redesign",
+      img: Academy,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus id voluptates debitis molestias accusantium?",
+    },
+    {
+      title: "Nike Redesign",
+      img: Academy,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus id voluptates debitis molestias accusantium?",
+    },
+    {
+      title: "Nike Redesign",
+      img: Academy,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus id voluptates debitis molestias accusantium?",
+    },
+    {
+      title: "Nike Redesign",
+      img: Academy,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus id voluptates debitis molestias accusantium?",
+    },
+    {
+      title: "Nike Redesign",
+      img: Academy,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus id voluptates debitis molestias accusantium?",
+    },
+  ]);
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 640, min: 0 },
+      items: 1,
+    },
+  };
+
+  const CustomRightArrow = ({ onClick, ...rest }) => {
+    const {
+      onMove,
+      carouselState: { currentSlide, deviceType },
+    } = rest;
+    // onMove means if dragging or swiping in progress.
+    return (
+      <button
+        className={`p-4 text-xl border text-stone-800 border-stone-800 rotate-180 absolute z-50
+         right-0 bottom-0 hover:text-white hover:bg-stone-800 transition hidden md:block`}
+        onClick={() => onClick()}
+      >
+        <BsArrowLeft />
+      </button>
+    );
+  };
+
+  const CustomLeftArrow = ({ onClick, ...rest }) => {
+    const {
+      onMove,
+      carouselState: { currentSlide, deviceType },
+    } = rest;
+    // onMove means if dragging or swiping in progress.
+    return (
+      <button
+        className="p-4 text-xl border text-stone-800 border-stone-800 absolute z-50
+         left-0 bottom-0 hover:text-white hover:bg-stone-800 transition hidden md:block"
+        onClick={() => onClick()}
+      >
+        <BsArrowLeft />
+      </button>
+    );
+  };
+
+  return (
+    <div className="mt-20 flex-row max-w-7xl mx-auto px-8">
+      <Parallax
+        strength={100}
+        renderLayer={(precentage) => (
+          <Carousel
+            responsive={responsive}
+            swipeable={true}
+            customRightArrow={<CustomRightArrow />}
+            customLeftArrow={<CustomLeftArrow />}
+            className="pb-0 md:pb-20"
+            infinite={false}
+          >
+            {projects.map((project, i) => (
+              <ProjectCard
+                title={project.title}
+                img={project.img}
+                desc={project.desc}
+                key={i}
+                duration={i}
+                perc={precentage}
+              ></ProjectCard>
+            ))}
+          </Carousel>
+        )}
+      ></Parallax>
+    </div>
+  );
+};
+
+export default ProjectSlider;
