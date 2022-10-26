18d487d126ef1ca538889c9b220b401d019705ba
Parsa <parsaasadpour999@gmail.com>
2022-10-26 21:06:48 +0330

    CV update

--- src/components/Header/ResumeButton.jsx
+++ src/components/Header/ResumeButton.jsx
@@ -11,8 +11,8 @@ const ScrollButton = () => {
       transition={{ delay: 2 }}
       className="px-4 py-3 sm:px-5 lg:px-8 md:py-3 text-sm md:text-lg border border-stone-800 mt-10 md:mt-20 relative cursor-pointer hover:bg-stone-800
        z-20 hover:text-slate-100 duration-300 flex flex-row items-center justify-between w-7/12 lg:w-1/2"
-      href="/My Resume.pdf"
-      download="My Resume.pdf"
+      href="/CV - Parsa Asadpour.pdf"
+      download="CV - Parsa Asadpour.pdf"
     >
       Download CV <FiArrowDown />
     </motion.a>
