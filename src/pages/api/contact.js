1fed92c1ea33faf47d15621ccba8962bf6166662
Parsa <parsaasadpour999@gmail.com>
2022-10-27 20:44:19 +0330

    nothing important

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,32 +1,32 @@
-import nodemailer from 'nodemailer'
+// import nodemailer from 'nodemailer'
 
-export default function handler(req, res) {
-  const {yourName, yourEmail, subject, message} = req.body
+// export default function handler(req, res) {
+//   const {yourName, yourEmail, subject, message} = req.body
   
-  const transporter = nodemailer.createTransport({
-      service: "gmail",
-      secure: true,
-      auth: {
-        user: process.env.gmailEmail,
-        pass: process.env.gmailPass,
-      },
-    });
+//   const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       secure: true,
+//       auth: {
+//         user: process.env.gmailEmail,
+//         pass: process.env.gmailPass,
+//       },
+//     });
 
-  const mailOptions = {
-    from: yourEmail,
-    to: "parsaasadpour1199@gmail.com",
-    subject: `${yourName}: ${subject}`,
-    text: message,
-  };
-  try{
-    const emailRes = transporter.sendMail({
-      from: yourEmail,
-      to: "parsaasadpour1199@gmail.com",
-      subject: `${yourName}: ${subject}`,
-      text: message,
-    })
-}
-catch (err) {
-  console.log(err, emailRes)
-}
-}
+//   const mailOptions = {
+//     from: yourEmail,
+//     to: "parsaasadpour1199@gmail.com",
+//     subject: `${yourName}: ${subject}`,
+//     text: message,
+//   };
+//   try{
+//     const emailRes = transporter.sendMail({
+//       from: yourEmail,
+//       to: "parsaasadpour1199@gmail.com",
+//       subject: `${yourName}: ${subject}`,
+//       text: message,
+//     })
+// }
+// catch (err) {
+//   console.log(err, emailRes)
+// }
+// }
