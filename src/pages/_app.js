385675c020b168a071164873eb023ef926f55569
Parsa <parsaasadpour.1999@gmail.com>
2023-02-16 14:11:30 +0330

    temporarily removed dynamic bg

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,14 +3,14 @@ import "../assets/styles/index.css";
 import ThemeContext from "../context/themeContext";
 
 function MyApp({ Component, pageProps }) {
-  const [theme, setTheme] = useState()
-  useEffect(() => {
-    fetch("/api/theme")
-      .then((res) => res.json())
-      .then((data) => {
-        setTheme(data.theme);
-      });
-  });
+  const [theme, setTheme] = useState(1)
+  // useEffect(() => {
+  //   fetch("/api/theme")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setTheme(data.theme);
+  //     });
+  // });
   return (
     <ThemeContext.Provider value={theme}>
       <Component {...pageProps} />
